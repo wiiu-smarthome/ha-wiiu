7cded76ebce7fda5bfe8faed2cff1577dd1f765e
Jordan <jordan@hrvy.uk>
2025-04-14 19:20:27 +0100

    Add binary sensor components for Wii U, including charging status

--- custom_components/nintendo_wiiu_ristretto/__init__.py
+++ custom_components/nintendo_wiiu_ristretto/__init__.py
@@ -6,7 +6,7 @@ from homeassistant.core import HomeAssistant
 
 from .coordinator import WiiUCoordinator
 
-PLATFORMS = [Platform.BUTTON, Platform.MEDIA_PLAYER, Platform.SENSOR]
+PLATFORMS = [Platform.BUTTON, Platform.MEDIA_PLAYER, Platform.SENSOR, Platform.BINARY_SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
