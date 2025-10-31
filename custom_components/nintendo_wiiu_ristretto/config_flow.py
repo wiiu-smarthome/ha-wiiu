f5015b111509839bd6ae8d1f90798062afaa0e46
pantherale0 <jordan@hrvy.uk>
2025-10-31 23:36:01 +0000

    Enhance Wii U integration with device info migration and improved error handling

--- custom_components/nintendo_wiiu_ristretto/config_flow.py
+++ custom_components/nintendo_wiiu_ristretto/config_flow.py
@@ -5,12 +5,19 @@ from typing import Any
 import voluptuous as vol
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import (
+    ATTR_HW_VERSION,
+    ATTR_MODEL,
+    ATTR_SERIAL_NUMBER,
+    ATTR_SW_VERSION,
     CONF_IP_ADDRESS,
     CONF_NAME,
     CONF_PORT,
     CONF_SCAN_INTERVAL,
     CONF_TIMEOUT,
 )
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from python_wiiu_ristretto import WiiU
 
 from .const import DEFAULT_TIMEOUT, DOMAIN
 
@@ -25,10 +32,30 @@ SCHEMA = vol.Schema(
 )
 
 
-class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
+async def get_device_info(
+    hass: HomeAssistant,
+    ip_addres: str,
+    port: int,
+    timeout: int
+) -> tuple[str, str, str, str]:
+    """Return device info from Wii U."""
+    wii = WiiU(
+        ip_address=ip_addres,
+        ristretto_port=port,
+        session=async_get_clientsession(hass),
+        timeout=timeout,
+    )
+    serial = await wii.async_get_device_serial_id()
+    hw_version = str(await wii.async_get_device_hardware_version())
+    model = await wii.async_get_device_model_number()
+    sw_version = await wii.async_get_device_version()
+    return serial, hw_version, model, sw_version
+
+
+class WiiUConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle flow."""
 
-    VERSION = 1
+    VERSION = 2
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -36,8 +63,28 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
         """User setup step."""
         errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
-
+            await self.async_set_unique_id(f"{DOMAIN}_{user_input[CONF_IP_ADDRESS]}")
+            self._abort_if_unique_id_configured()
+            try:
+                serial, hw_version, model, sw_version = await get_device_info(
+                    hass=self.hass,
+                    ip_addres=user_input[CONF_IP_ADDRESS],
+                    port=user_input[CONF_PORT],
+                    timeout=user_input[CONF_TIMEOUT],
+                )
+            except (TimeoutError, ConnectionError):
+                errors["base"] = "cannot_connect"
+            else:
+                user_input = {
+                    **user_input,
+                    ATTR_SERIAL_NUMBER: serial,
+                    ATTR_HW_VERSION: hw_version,
+                    ATTR_MODEL: model,
+                    ATTR_SW_VERSION: sw_version,
+                }
+                return self.async_create_entry(
+                    title=user_input[CONF_NAME], data=user_input
+                )
         return self.async_show_form(
             step_id="user",
             data_schema=SCHEMA,
--- custom_components/nintendo_wiiu_ristretto/coordinator.py
+++ custom_components/nintendo_wiiu_ristretto/coordinator.py
@@ -13,7 +13,7 @@ from homeassistant.const import (
     CONF_TIMEOUT,
 )
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from python_wiiu_ristretto import WiiU
 
@@ -27,12 +27,8 @@ class WiiUCoordinator(DataUpdateCoordinator):
 
     gamepad_charging: bool = False
     gamepad_battery: int = None
-    serial: str = None
     source_list: list = None
     source: str = None
-    model: str = None
-    hw_version: str = None
-    sw_version: str = None
     wii: WiiU = None
     title_map: dict = None
 
@@ -55,13 +51,6 @@ class WiiUCoordinator(DataUpdateCoordinator):
         self._ristretto_port = config_entry.data[CONF_PORT]
         self.is_on = False
 
-    async def async_get_hardware_information(self) -> None:
-        """Get hardware information."""
-        self.serial = await self.wii.async_get_device_serial_id()
-        self.hw_version = await self.wii.async_get_device_hardware_version()
-        self.model = await self.wii.async_get_device_model_number()
-        self.sw_version = await self.wii.async_get_device_version()
-
     async def _get_current_app_name(self) -> None:
         """Get the current app name."""
         self.source = await self.wii.async_get_current_title_name()
@@ -76,15 +65,15 @@ class WiiUCoordinator(DataUpdateCoordinator):
         self.wii = WiiU(
             ip_address=self._ip_address,
             ristretto_port=self._ristretto_port,
-            session=async_create_clientsession(self.hass),
+            session=async_get_clientsession(self.hass),
             timeout=self.config_entry.data.get(CONF_TIMEOUT, DEFAULT_TIMEOUT),
         )
-        await self.async_get_hardware_information()
-        await self._get_source_list()
 
     async def _async_update_data(self) -> None:
         """Update the data from the Wii U."""
         try:
+            if self.title_map is None:
+                await self._get_source_list()
             await self._get_current_app_name()
             self.gamepad_battery = await self.wii.async_get_gamepad_battery()
             if isinstance(self.gamepad_battery, int):
